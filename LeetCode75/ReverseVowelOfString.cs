1e15324488dc1d2ad3ec6756d0c03eb6023dd3c0
ygeorgaras <ygeorgaras@gmail.com>
2024-01-29 23:23:24 -0500

    Reverse Vowels of a String
    https://leetcode.com/problems/reverse-vowels-of-a-string/submissions/1160679155/?envType=study-plan-v2&envId=leetcode-75

---
+++ LeetCode75/ReverseVowelOfString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.LeetCode75
+{
+    internal class ReverseVowelOfString
+    {
+        //Reverses only vowels in a string
+        public static string reverseVowels(string s)
+        {
+            var cArray = new List<char>();
+            string newString = string.Empty;
+
+            foreach (var c in s)
+            {
+                if("aeiouAEIOU".Contains(c))
+                {
+                    cArray.Add(c);
+                }
+            }
+
+            int cArrayLength = cArray.Count;
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if ("aeiouAEIOU".Contains(s[i]))
+                {
+                    newString += cArray[cArrayLength - 1];
+                    cArrayLength--;
+                }
+                else
+                {
+                    newString += s[i];
+                }
+            }
+            return newString;
+        }
+    }
+}
