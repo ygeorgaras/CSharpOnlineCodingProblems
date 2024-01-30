a438ee55ef54205019b9eed1e5ac68920d8625e2
ygeorgaras <ygeorgaras@gmail.com>
2024-01-29 23:01:13 -0500

    CanPlaceFlowers
    https://leetcode.com/problems/can-place-flowers/submissions/1160661482/?envType=study-plan-v2&envId=leetcode-75

---
+++ LeetCode75/CanPlaceFlowers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodingProblems.LeetCode75
+{
+    internal class CanPlaceFlowers
+    {
+        public static bool PlaceFlowers(int[] flowerbed, int n)
+        {
+            for(int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0)
+                {
+                    //If i is 0 then there is nothing to the left
+                    var emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                    //If i is the length-1 then there is nothing to the right
+                    var emptyRightPlot = (i == flowerbed.Length - 1) || (flowerbed[i + 1] == 0);
+                    if(emptyLeftPlot && emptyRightPlot)
+                    {
+                        flowerbed[i] = 1;
+                        n--;
+                    }
+                }
+                if(n <= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- LeetCode75/LeetCode.cs
+++ LeetCode75/LeetCode.cs
@@ -12,7 +12,8 @@ namespace CodingProblems.LeetCode75
             //MergeAlternately.mergeAlternately("helloasdfasdf", "hi");
             //GreatestCommonDivisorOfStrings.GcdOfStrings("ABABAB", "ABAB");
             //ExtraCandies.KidsWithCandies(new int[] { 3, 1, 4, 5, 2 }, 2);
-            MaximumAverageSubarray.FindMaxAverage(new int[] { 1, 12, -5, -6, 50, 3 }, 4);
+            //MaximumAverageSubarray.FindMaxAverage(new int[] { 1, 12, -5, -6, 50, 3 }, 4);
+            Console.WriteLine(CanPlaceFlowers.PlaceFlowers(new int[] { 0, 0, 0, 0, 0, 1, 0, 0 }, 0));
         }
     }
 }
