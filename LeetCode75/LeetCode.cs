2e5b260034ae677008aaacba21944564791c46bd
ygeorgaras <ygeorgaras@gmail.com>
2024-02-10 00:25:16 -0500

    MaxVowels and Sliding Window
    https://leetcode.com/problems/max-consecutive-ones-iii/submissions/1171142206/?envType=study-plan-v2&envId=leetcode-75

--- LeetCode75/LeetCode.cs
+++ LeetCode75/LeetCode.cs
@@ -23,7 +23,8 @@ namespace CodingProblems.LeetCode75
             //Console.Write(IsSubsequence.isSubsequence("", ""));
             //Console.Write(MaxOperations.maxOper(new int[] { 3, 1, 3, 4, 3 }, 6));
             //Console.Write(MaxOperations.maxOper(new int[] { 2, 5, 4, 4, 1, 3, 4, 4, 1, 4, 4, 1, 2, 1, 2, 2, 3, 2, 4, 2 }, 3));
-            Console.WriteLine(MaxVowels.maxVowelsInString("tryhard", 4));
+            //Console.WriteLine(MaxVowels.maxVowelsInString("tryhard", 4));
+            Console.WriteLine(LongestZero.longestSet(new int[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, 2));
         }
     }
 }
---
+++ LeetCode75/LongestZero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.LeetCode75
+{
+    internal class LongestZero
+    {
+        public static int longestSet(int[] nums, int k)
+        {
+            int left = 0;
+            int curr = 0;
+            int ans = 0;
+            for(int right = 0; right < nums.Length; right++)
+            {
+                if (nums[right] == 0)
+                {
+                    curr++;
+                }
+
+                while(curr > k)
+                {
+                    if (nums[left] == 0)
+                    {
+                        curr--;
+                    }
+                    left++;
+                }
+                ans = Math.Max(ans, right - left + 1);
+            }
+            return ans;
+        }
+    }
+}
